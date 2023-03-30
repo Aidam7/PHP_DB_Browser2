6e95bc030d6ed1577f401e7e66b6a9176c4d9883
Aidam <55688345+Aidam7@users.noreply.github.com>
2023-03-30 10:40:40 +0200

    credentials attempts

--- public/staff/changeCredentials.php
+++ public/staff/changeCredentials.php
@@ -4,29 +4,34 @@ require_once __DIR__ . "/../../bootstrap/bootstrap.php";
 class CredentialsChangePage extends CRUDPage
 {
     protected Staff $user;
+    private int $employeeId;
     protected function prepare(): void
     {
         parent::prepare();
 
-        $employeeId = $_SESSION['user'];
+        $this->employeeId = filter_input(INPUT_POST, 'employeeId');
         $newPassword = filter_input(INPUT_POST, 'password');
+
         $oldPassword = filter_input(INPUT_POST, 'oldPassword');
         $login = filter_input(INPUT_POST, 'login');
-        if (!$employeeId)
-            throw new BadRequestException();
 
         //Should be impossible but just in case
-        if($_SESSION['user'] != $employeeId)
+        if($_SESSION['user'] != $this->employeeId)
             throw new AccessDeniedException();
+        if($newPassword != null)
+            $newPassword = password_hash($newPassword,PASSWORD_DEFAULT);
         $this->user = Staff::findByID($_SESSION['user']);
-        if(/*password_verify(*/$oldPassword/*,PASSWORD_DEFAULT)*/ == $this->user->password && $this->user->login){
-            $query = $stmt = PDOProvider::get()->prepare("UPDATE ".Staff::DB_TABLE." SET `login` = :login, `password` = :password WHERE `employee_id` = :employeeId");
+        if(password_verify($oldPassword, $this->user->password) && $this->user->login == $login){
+            $query ="UPDATE ".Staff::DB_TABLE." SET `login` = :login, `password` = :password WHERE `employee_id` = :employeeId";
             $stmt = PDOProvider::get()->prepare($query);
-            $stmt->execute([
+
+            if($stmt->execute([
                 'login'=>$login,
-                'password'=>password_hash($newPassword,PASSWORD_DEFAULT, array('cost' => 15)),
-                'employee_id'=>$employeeId
-                ]);
+                'password'=>$newPassword,
+                'employeeId'=>$this->employeeId
+                ])){
+
+            }
         }
 
 
@@ -37,7 +42,7 @@ class CredentialsChangePage extends CRUDPage
 
     protected function pageBody()
     {
-        $html =  MustacheProvider::get()->render('changeLoginForm',["login"=>$this->user->login]);
+        $html =  MustacheProvider::get()->render('changeLoginForm',["login"=>$this->user->login, "employeeId" => $this->employeeId]);
         return $html;
     }
 
